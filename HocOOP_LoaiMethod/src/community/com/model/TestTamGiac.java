c64a7e8fb2b72bb8c8a165dc8a934dd90c07accd
ACER <ACER@KhoaNguyen>
2020-02-24 21:51:15 +0700

    Bai63_HocOOPLoaiMethod

---
+++ HocOOP_LoaiMethod/src/community/com/model/TestTamGiac.java
@@ -0,0 +1,12 @@
+package community.com.model;
+
+public class TestTamGiac {
+
+	public static void main(String[] args) {
+		TamGiac tg1 = new TamGiac(6,7,8);
+		System.out.println("Chu vi = " + tg1.tinhChuVi());
+		System.out.println("Dien tich = " + tg1.dienTich());
+
+	}
+
+}
